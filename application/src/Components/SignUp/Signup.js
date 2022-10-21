7b6c0450648c884ae180d706ff6ac9aed78f1036
Krishna Paudel <krisnachhetri07@gmail.com>
2022-10-21 13:03:12 +1100

    comit

--- application/src/Components/SignUp/Signup.js
+++ application/src/Components/SignUp/Signup.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import SignUpContainer from "./SignUpContainer";
+// import SignUpContainer from "./SignUpContainer";
+import SignUpForm from "./SignUpForm";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    pwConfirm: "",
+    err: "",
+  });
+  const { name, email, password, pwConfirm, err } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const authData = () => {
+    if (!name) {
+      return (err = "Please enter the username");
+    }
+    console.log(formData);
+  };
+
   return (
     <MuiThemeProvider>
-      <SignUpContainer />
+      <SignUpForm onChange={onChange} formData={formData} />
     </MuiThemeProvider>
   );
 };
